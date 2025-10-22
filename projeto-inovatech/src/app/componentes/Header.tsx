af85e200d2ecfb5e8b9ae5a66ef29187365ac5cb
HENRYEZ97 <ricksillva97@gmail.com>
2025-10-22 12:01:43 -0400

    💄 BG-blue adicionado ao Header.

--- projeto-inovatech/src/app/componentes/Header.tsx
+++ projeto-inovatech/src/app/componentes/Header.tsx
@@ -6,10 +6,10 @@ interface HeaderProps {
 
 export default function Header({ onMenuClick }: HeaderProps) {
   return (
-    <header className="flex items-center justify-between bg-white px-6 py-4 shadow-sm border-b border-gray-200">
+    <header className="flex items-center justify-between  bg-gradient-to-br from-blue-300 to blue-600 px-6 py-4 shadow-sm border-b border-gray-200">
         <button
           onClick={onMenuClick}
-          className="text-slate-600 hover:text-blue-500 transition-colors hover:bg-blue-50 p2-rounded-lg cursor-pointer"
+          className="text-slate-600 hover:text-blue-500 transition-colors hover:bg-gray-100 p2-rounded-lg cursor-pointer"
            >
           <Menu size={28} />
         </button>
